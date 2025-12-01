eeac6bbaf509096f282b02e91c2f62ab0394ae87
James Harmison <jharmison@redhat.com>
2025-12-01 13:12:01 -0500

    add: console link for grafana

---
+++ charts/grafana/files/consolelink/create-consolelink.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -ex
+
+ROUTE=$(oc get route -n grafana grafana-route -ojsonpath='{.status.ingress[0].host}')
+export ROUTE
+
+cat << EOF | oc apply -f-
+$(cat /mnt/consolelink.yaml.tpl)
+EOF
