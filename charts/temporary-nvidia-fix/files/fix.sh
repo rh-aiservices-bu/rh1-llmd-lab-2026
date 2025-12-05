963f061c1fa6506e2acd056c0af28187abad1f85
James Harmison <jharmison@redhat.com>
2025-12-05 15:19:00 -0500

    fix: prettify output of job

--- charts/temporary-nvidia-fix/files/fix.sh
+++ charts/temporary-nvidia-fix/files/fix.sh
@@ -1,25 +1,40 @@
 #!/bin/bash
 
-set -ex
+set -e
 
-# Clean up the ClusterPolicy
+echo "Removing cluster policy"
 oc delete clusterpolicy gpu-cluster-policy --wait=true ||:
+echo -n "Waiting for cleanup"
 while ! oc get pod -n nvidia-gpu-operator -l app.kubernetes.io/component!=gpu-operator,batch.kubernetes.io/job-name!=fix-gpu-operator |& grep -qF 'No resources found'; do
+  echo -n .
   sleep 5
 done
+echo
 
-# Clean up the operator
+echo "Uninstalling the operator"
 oc delete subscription -n nvidia-gpu-operator gpu-operator-certified --wait=true ||:
 oc delete csv -n nvidia-gpu-operator -l operators.coreos.com/gpu-operator-certified.nvidia-gpu-operator --wait=true ||:
+echo -n "Waiting for cleanup"
 while ! oc get pod -n nvidia-gpu-operator -l batch.kubernetes.io/job-name!=fix-gpu-operator |& grep -qF 'No resources found'; do
+  echo -n .
   sleep 5
 done
+echo
 
-# Install the correct operator
+echo "Installing the new operator"
 oc apply -f /app/subscription.yaml
+echo -n "Waiting for operator installation"
 while [ "$(oc get subscription -n nvidia-gpu-operator gpu-operator-certified -ojsonpath='{.status.state}')" != "AtLatestKnown" ]; do
+  echo -n .
   sleep 5
 done
+echo
+echo -n "Waiting for operator pod to be ready"
+while [ "$(oc get pod -n nvidia-gpu-operator -l app.kubernetes.io/component=gpu-operator -ojsonpath='{.items[0].status.conditions[?(@.type=="Ready")].status}')" != "True" ]; do
+  echo -n .
+  sleep 5
+done
+echo
 
-# Apply the correct ClusterPolicy
+echo "Creating new ClusterPolicy"
 oc apply -f /app/clusterpolicy.yaml
